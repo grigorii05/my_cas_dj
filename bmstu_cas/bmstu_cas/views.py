534731a2f6001e048cc87c2755b292ec510f1e97
grigorii05 <47519076+grigorii05@users.noreply.github.com>
2022-07-06 14:58:43 +0300

    Update views.py

--- bmstu_cas/bmstu_cas/views.py
+++ bmstu_cas/bmstu_cas/views.py
@@ -1,8 +1,10 @@
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 
 def index(request):
     if request.user.is_authenticated:
-        return HttpResponse('<p>Вы вошли как <strong>%s</strong>.</p><p><a href="/cas/logout">Выйти</a></p>' % request.user)
+         redirect('http://guimc-dev.eu.bmstu.ru/api/registration/?ticket=ST-85631-zsP2W3qqmc1gAvhsGze6-proxy')
+        #return HttpResponse('<p>Вы вошли как <strong>%s</strong>.</p><p><a href="/cas/logout">Выйти</a></p>' % request.user)
     else:
-        return HttpResponse('<a href="https://proxy.bmstu.ru:8443/cas/login?service=http%3A%2F%2Fguimc-dev.eu.bmstu.ru%2Fapi%2Fregistration%2F">Войти</a>')
+        return HttpResponse('<a href="https://proxy.bmstu.ru:8443/cas/login">Войти</a>')
